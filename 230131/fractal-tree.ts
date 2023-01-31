83545123e48819e3714a0febd556985289f6f207
Roland Nagy <roland@cimem.hu>
2023-01-31 21:45:27 +0100

    changed branch number, added leaves

--- 230131/fractal-tree.ts
+++ 230131/fractal-tree.ts
@@ -29,30 +29,35 @@ class Point {
   }
 }
 
-function drawLine(
+function endPointCalulator(
   startPoint: Point,
   length: number,
-  degree: number,
-  strength: number,
-  ctx: CanvasRenderingContext2D
+  degree: number
 ): Point {
   //calculating line endpoint
-  const xCount: number = Math.cos(((Math.PI * 2) / 360) * degree) * length;
-  const yCount: number = Math.sin(((Math.PI * 2) / 360) * -degree) * length;
+  const xShift: number = Math.cos(((Math.PI * 2) / 360) * degree) * length;
+  const yShift: number = Math.sin(((Math.PI * 2) / 360) * -degree) * length;
+
   const endPoint: Point = new Point(
-    startPoint.getX() + xCount,
-    startPoint.getY() + yCount
+    startPoint.getX() + xShift,
+    startPoint.getY() + yShift
   );
 
+  return endPoint;
+}
+
+function drawLine(
+  startPoint: Point,
+  endPoint: Point,
+  lineWidth: number,
+  ctx: CanvasRenderingContext2D
+) {
   ctx.beginPath();
   ctx.moveTo(startPoint.getX(), startPoint.getY());
-  ctx.lineTo(startPoint.getX() + xCount, yCount + startPoint.getY());
+  ctx.lineTo(endPoint.getX(), endPoint.getY());
   ctx.strokeStyle = 'white';
-  ctx.lineWidth = strength;
+  ctx.lineWidth = lineWidth;
   ctx.stroke();
-
-  //returning endpoint for further operation
-  return endPoint;
 }
 
 function drawTree(
@@ -63,27 +68,53 @@ function drawTree(
   ctx: CanvasRenderingContext2D
 ) {
   // setting up default tree characteristic
-  const branchDegree: number = 25;
+  const branchDegree: number = 20;
   const growLimit: number = 0.8;
+  const branchEndPoint: Point = endPointCalulator(startPoint, length, degree);
 
-  //draw one branch and after that calls the branchgrowing three times, if its not the last one
-  const endPoint: Point = drawLine(startPoint, length, degree, level + 1, ctx);
-  if (level > 0) {
+  //calls the branchgrowing five times, if its not the last one
+
+  if (level !== 0) {
     drawTree(
       level - 1,
-      endPoint,
-      degree - branchDegree,
+      branchEndPoint,
+      degree - branchDegree * 2 - 10,
       length * growLimit,
       ctx
     );
-    drawTree(level - 1, endPoint, degree, length * growLimit, ctx);
     drawTree(
       level - 1,
-      endPoint,
-      degree + branchDegree,
+      branchEndPoint,
+      degree - branchDegree - 5,
       length * growLimit,
       ctx
     );
+    drawTree(level - 1, branchEndPoint, degree, length * growLimit, ctx);
+    drawTree(
+      level - 1,
+      branchEndPoint,
+      degree + branchDegree + 5,
+      length * growLimit,
+      ctx
+    );
+    drawTree(
+      level - 1,
+      branchEndPoint,
+      degree + branchDegree * 2 + 10,
+      length * growLimit,
+      ctx
+    );
+  }
+
+  //draws a branch
+  drawLine(startPoint, branchEndPoint, level + 1, ctx);
+
+  //if it is the last branch, draws a leaf
+  if (level === 0) {
+    ctx.beginPath();
+    ctx.fillStyle = 'darkgreen';
+    ctx.arc(branchEndPoint.getX(), branchEndPoint.getY(), 5, 0, Math.PI * 2);
+    ctx.fill();
   }
 }
 
@@ -94,7 +125,7 @@ const initialGrowingPoint: Point = new Point(
 );
 const initialGrowingDegree: number = 90;
 const initialGrowingDistance = canvas.height / 5;
-const initialGrowingLevels = 8;
+const initialGrowingLevels = 5;
 ctx.beginPath();
 ctx.fillStyle = 'midnightblue';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
